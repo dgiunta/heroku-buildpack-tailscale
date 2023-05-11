0afadc831f3882fbddbbafcf812c7964fea606e4
Dave Giunta <697+dgiunta@users.noreply.github.com>
2023-05-11 18:49:27 -0500

    adding explicit http proxy listening configurations

--- bin/start_tailscale.sh
+++ bin/start_tailscale.sh
@@ -2,8 +2,11 @@
 
 TAILSCALE_PROXY_PORT=${TAILSCALE_PROXY_PORT:-1055}
 
-/app/bin/tailscaled --tun=userspace-networking --socks5-server=localhost:"$TAILSCALE_PROXY_PORT" &
+/app/bin/tailscaled --tun=userspace-networking --socks5-server=localhost:"$TAILSCALE_PROXY_PORT" --outbound-http-proxy-listen=localhost:"$TAILSCALE_PROXY_PORT" &
+
 /app/bin/tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname=heroku-app --accept-routes
 
 echo Tailscale started
 export ALL_PROXY=socks5://localhost:"$TAILSCALE_PROXY_PORT"
+export HTTP_PROXY=http://localhost:"$TAILSCALE_PROXY_PORT"
+export http_proxy=http://localhost:"$TAILSCALE_PROXY_PORT"
