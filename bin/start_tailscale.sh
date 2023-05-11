c9cbccdf421a2563b481934bbd569d476e54afd9
Dave Giunta <697+dgiunta@users.noreply.github.com>
2023-05-11 17:24:43 -0500

    fixing some things from shellcheck and adding the ability to ignore cache when running the docker build script for updating tailscale versions

--- bin/start_tailscale.sh
+++ bin/start_tailscale.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-/bin/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-/bin/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=heroku-app
+TAILSCALE_PROXY_PORT=${TAILSCALE_PROXY_PORT:-1055}
+
+/app/bin/tailscaled --tun=userspace-networking --socks5-server=localhost:"$TAILSCALE_PROXY_PORT" &
+/app/bin/tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname=heroku-app --accept-routes
+
 echo Tailscale started
-export ALL_PROXY=socks5://localhost:1055
+export ALL_PROXY=socks5://localhost:"$TAILSCALE_PROXY_PORT"
