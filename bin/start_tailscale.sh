a7ec54065dd821d1f581f755fbc018b25841efe4
Dave Giunta <697+dgiunta@users.noreply.github.com>
2023-05-12 13:37:32 -0500

    prefix the tailscale started message

--- bin/start_tailscale.sh
+++ bin/start_tailscale.sh
@@ -18,7 +18,8 @@ TAILSCALE_PROXY_PORT=${TAILSCALE_PROXY_PORT:-1055}
 
 /app/bin/tailscale up --authkey="$TAILSCALE_AUTHKEY" --hostname=heroku-app --accept-routes
 
-echo Tailscale started
 export ALL_PROXY=socks5://localhost:"$TAILSCALE_PROXY_PORT"
 export HTTP_PROXY=http://localhost:"$TAILSCALE_PROXY_PORT"
 export http_proxy=http://localhost:"$TAILSCALE_PROXY_PORT"
+
+echo "Tailscale tunnel started" | prefix
